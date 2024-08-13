c42db13b3aba32ba52a52ae02180a8be0a9fbe51
fawda123 <mbafs2012@gmail.com>
2024-08-13 13:45:03 -0400

    working on baywide reactives

--- R/global.R
+++ R/global.R
@@ -4,6 +4,7 @@ box::use(
 
 data('fibdata', package = 'tbeptools')
 data('enterodata', package = 'tbeptools')
+data('catchprecip', package = 'tbeptools')
 
 cols <- c('#CC3231', '#E9C318', '#2DC938')
 
@@ -12,13 +13,63 @@ maxyr <- 2023
 mos <- as.list(1:12)
 names(mos) <- month.abb
 
-areas <- c('Alafia River', 'Hillsborough River', 'Big Bend', 'Cockroach Bay', 
+areas1 <- c("Boca Ciega Bay", "Hillsborough Bay", "Lower Tampa Bay", "Manatee River", 
+            "Middle Tampa Bay", "Old Tampa Bay")
+areas2 <- c('Alafia River', 'Hillsborough River', 'Big Bend', 'Cockroach Bay', 
            'East Lake Outfall', 'Hillsborough Bay', 'Little Manatee River', 'Lower Tampa Bay',
            'McKay Bay', 'Middle Tampa Bay', 'Old Tampa Bay', 'Palm River', 'Tampa Bypass Canal',
            'Valrico Lake')
 
+
 # create custom icon list for fib categories
-icons <- leaflet::iconList(
+ecocciicons <- leaflet::iconList(
+  ecocci_green_wet <- leaflet::makeIcon(iconUrl = system.file('ecoli_green.png', package = 'tbeptools'),
+                                        iconWidth = 18, iconHeight = 18),
+  ecocci_yellow_wet <- leaflet::makeIcon(iconUrl = system.file('ecoli_yellow.png', package = 'tbeptools'),
+                                         iconWidth = 18, iconHeight = 18),
+  ecocci_orange_wet <- leaflet::makeIcon(iconUrl = system.file('ecoli_orange.png', package = 'tbeptools'),
+                                         iconWidth = 18, iconHeight = 18),
+  ecocci_red_wet <- leaflet::makeIcon(iconUrl = system.file('ecoli_red.png', package = 'tbeptools'),
+                                      iconWidth = 18, iconHeight = 18),
+  ecocci_green_dry <- leaflet::makeIcon(iconUrl = system.file('ecocci_green.png', package = 'tbeptools'),
+                                        iconWidth = 18, iconHeight = 18),
+  ecocci_yellow_dry <- leaflet::makeIcon(iconUrl = system.file('ecocci_yellow.png', package = 'tbeptools'),
+                                         iconWidth = 18, iconHeight = 18),
+  ecocci_orange_dry <- leaflet::makeIcon(iconUrl = system.file('ecocci_orange.png', package = 'tbeptools'),
+                                         iconWidth = 18, iconHeight = 18),
+  ecocci_red_dry <- leaflet::makeIcon(iconUrl = system.file('ecocci_red.png', package = 'tbeptools'),
+                                      iconWidth = 18, iconHeight = 18)
+  
+)
+
+# legend as HTML string
+# using previously created code and icons that have ecoli in the name -
+# so monkeying around a bit to make shapes match correctly
+ecoccilevs <- tbeptools::util_fiblevs()
+ecoccileg <- tibble::tibble(
+  src = paste0('https://github.com/tbep-tech/tbeptools/blob/master/inst/', basename(sapply(ecocciicons, `[[`, 1)), '?raw=true'),
+  brk = rep(ecoccilevs$ecoccilbs, times = 2)
+) %>%
+  tidyr::unite('val', src, brk, sep = "' style='width:10px;height:10px;'> ") %>%
+  dplyr::mutate(
+    val = paste0("<img src='", val)
+  ) %>%
+  dplyr::pull(val)
+ecoccidryleg <- ecoccileg %>%
+  grep('ecoli', ., value = T) %>%
+  paste(collapse = '<br/>') %>%
+  paste0('<b>Dry samples</b><br/>#/100mL<br/>', .)
+ecocciwetleg <- ecoccileg %>%
+  grep('ecocci', ., value = T) %>%
+  paste(collapse = '<br/>') %>%
+  paste0('<b>Wet samples</b><br/>#/100mL<br/>', .)
+ecocciallleg <- ecoccileg %>%
+  grep('ecoli', ., value = T) %>%
+  paste(collapse = '<br/>') %>%
+  paste0('<b>All samples</b><br/>#/100mL<br/>', .)
+
+# create custom icon list for fib categories
+fibicons <- leaflet::iconList(
   ecoli_green <- leaflet::makeIcon(iconUrl = system.file('ecoli_green.png', package = 'tbeptools'),
                                    iconWidth = 18, iconHeight = 18),
   ecoli_yellow <- leaflet::makeIcon(iconUrl = system.file('ecoli_yellow.png', package = 'tbeptools'),
@@ -38,21 +89,21 @@ icons <- leaflet::iconList(
 )
 
 # legend as HTML string
-levs <- tbeptools::util_fiblevs()
-leg <- tibble::tibble(
-  src = paste0('https://github.com/tbep-tech/tbeptools/blob/master/inst/', basename(sapply(icons, `[[`, 1)), '?raw=true'),
-  brk = c(levs$ecolilbs, levs$ecoccilbs)
+fiblevs <- tbeptools::util_fiblevs()
+fibleg <- tibble::tibble(
+  src = paste0('https://github.com/tbep-tech/tbeptools/blob/master/inst/', basename(sapply(fibicons, `[[`, 1)), '?raw=true'),
+  brk = c(fiblevs$ecolilbs, fiblevs$ecoccilbs)
 ) %>%
   tidyr::unite('val', src, brk, sep = "' style='width:10px;height:10px;'> ") %>%
   dplyr::mutate(
     val = paste0("<img src='", val)
   ) %>%
   dplyr::pull(val)
-ecolileg <- leg %>%
+ecolileg <- fibleg %>%
   grep('ecoli', ., value = T) %>%
   paste(collapse = '<br/>') %>%
   paste0('<b>Freshwater (<em>E. Coli</em>)</b><br/>#/100mL<br/>', .)
-ecoccileg <- leg %>%
+ecoccileg <- fibleg %>%
   grep('ecocci', ., value = T) %>%
   paste(collapse = '<br/>') %>%
   paste0('<b>Marine (<em>Enterococcus</em>)</b><br/>#/100mL<br/>', .)
