48866c548e6297d72678325522df5531a3d01f5a
fawda123 <mbafs2012@gmail.com>
2024-10-13 11:00:01 -0400

    initial manco data add, not working

--- R/global.R
+++ R/global.R
@@ -2,8 +2,9 @@ box::use(
   dplyr[`%>%`]
 )
 
-data('fibdata', package = 'tbeptools')
 data('enterodata', package = 'tbeptools')
+data('fibdata', package = 'tbeptools')
+data('mancofibdata', package = 'tbeptools')
 data('catchprecip', package = 'tbeptools')
 data('tbsegdetail', package = 'tbeptools')
 
@@ -27,6 +28,14 @@ areas2 <- c('Alafia River', 'Hillsborough River', 'Big Bend', 'Cockroach Bay',
            'McKay Bay', 'Middle Tampa Bay', 'Old Tampa Bay', 'Palm River', 'Tampa Bypass Canal',
            'Valrico Lake')
 
+areas3 <- c("Big Slough", "Bowlees Creek", "Braden River", "Bud Slough", 
+            "Cedar Creek", "Clay Gully", "Cooper Creek", "Curiosity Creek", 
+            "Frog Creek", "Gamble Creek", "Gap Creek", "Gates Creek", "Gilley Creek", 
+            "Hickory Hammock Creek", "Lake Manatee", "Little Manatee River", 
+            "Lower Manatee River", "Lower Tampa Bay", "Manatee River Estuary", 
+            "Mcmullen Creek", "Mill Creek", "Mud Lake Slough", "Myakka River", 
+            "Nonsense Creek", "Palma Sola Bay", "Piney Point Creek", "Rattlesnake Slough", 
+            "Sugarhouse Creek", "Upper Manatee River", "Ward Lake", "Williams Creek")
 
 # create custom icon list for fib categories
 ecocciicons <- tbeptools::util_fibicons('entero')
