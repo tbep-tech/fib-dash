5775679396c5fe1660f0f7587572d5d6ff7475a4
fawda123 <mbafs2012@gmail.com>
2024-10-28 15:57:53 -0400

    working full plotly in navset_card_underline

--- R/funcs.R
+++ R/funcs.R
@@ -10,7 +10,10 @@ mataddyr_fun <- function(p, yrsel, lwd = 1.5){
     ggplot2::geom_segment(ggplot2::aes(x = maxx + 0.5, xend = maxx + 0.5, y = yrsel - 0.5, yend = yrsel + 0.5, lwd = lwd))
   
   out <- tbeptools::show_matrixplotly(p) |> 
-    plotly::config(displaylogo = F, modeBarButtonsToRemove = modrm)
+    plotly::config(
+      displaylogo = F,
+      modeBarButtonsToRemove = modrm
+      )
   
   return(out)
   
--- app.R
+++ app.R
@@ -71,22 +71,22 @@ ui <- page_navbar(
   nav_panel(
     class = 'fill-height',
     title = "OVERVIEW",
-    div(
-      class = 'resizable-row',
-      div(
-        class = 'resizable-column',
-        style = 'width: 66.66%',
-        card(
-          card_header("USING THE DASHBOARD"),
-          full_screen = TRUE,
-          height = '100%',
-          div(
-            class = 'card-scroll',
-            markdown(
-"            
+    navset_card_underline(
+      full_screen = TRUE,
+      height = '100%',
+      nav_panel(
+        title = "USING THE DASHBOARD",
+        class = 'card-scroll',
+        markdown(
+"          
+<div class = 'row'>
+<div class = 'col-md-2'></div>
+<div class = 'col-md-8'>
+
 <h2 style = 'text-align: center;'>WELCOME TO THE TAMPA BAY FIB DASHBOARD!</h2>
             
-<img src='bannerimage.png' style='width: 80%; display: block; margin: 0 auto;'>                         
+<img src='bannerimage.png' style='width: 80%; display: block; margin: 0 auto;'>  
+<br>
             
 This dashboard summarizes fecal indicator bacteria (FIB) data for a baywide assessment and for data collected as part of the Environmental Protection Commission of Hillsborough County (EPCHC) monitoring program.  The latter is specific to evaluating fecal impairments in the Hillsborough and Alafia river basins.  The assessments are meant to inform progress remediating fecal impairments or to support prioritization of areas for further investigation. They are not meant to support beach monitoring efforts or closures for recreational uses - alternative reporting products are available for that purpose (see <a href='https://www.floridahealth.gov/environmental-health/beach-water-quality/county-detail.html?County=Pinellas&Zip=33701-3109' target = '_blank'>FLDOH Healthy Beaches</a> and <a href='https://pinellas.wateratlas.usf.edu/maps/coastal-water-quality-map/' target='_blank'>Pinellas County Recreational Water Quality Map</a>). The dashboard is organized in the following sections:
             
@@ -98,7 +98,8 @@ This dashboard summarizes fecal indicator bacteria (FIB) data for a baywide asse
 The plots in this dashboard are interactive and display options can be controlled using a mouse. Most plots include a <a href='https://help.plot.ly/zoom-pan-hover-controls/' target='_blank'>control menu</a> on the top with different options for viewing the data.  For example, click the camera icon to download a plot.
                     
 <img src='plotcontrols.PNG' style='width: 30%; display: block; margin: 0 auto;'>   
-                      
+<br>
+
 <h2 style = 'text-align: center;'>DATA SOURCES</h2>
                       
 Source data used on this website were obtained from multiple sources for the baywide assessment and from the Environmental Protection Commission of Hillsborough County. Data can be obtained using functions from the tbeptools R package described below. Graphics and tables provided on the dashboard are made available for exploratory purposes only.  
@@ -114,21 +115,21 @@ Questions and comments about the dashboard can be sent to [Marcus Beck](mailto:m
 Citation info here: <a href='https://doi.org/10.5281/zenodo.13881473' target='_blank'><img src='https://zenodo.org/badge/841089887.svg' alt='DOI'></a>
                       
 <a rel='license' href='http://creativecommons.org/licenses/by/4.0/' target='_blank'><img alt='Creative Commons License' style='border-width:0' src='https://i.creativecommons.org/l/by/4.0/88x31.png' /></a>&nbsp;&nbsp;This website is licensed under a <a rel='license' href='http://creativecommons.org/licenses/by/4.0/' target='_blank'>Creative Commons Attribution 4.0 International License</a>.
+
+</div>
+<div class = 'col-md-2'></div>
+</div>
 "
-        )
-      ))
-    ),
-    div(
-      class='right-column', 
-      style = 'width: 33.33%',
-      card(
-        height = '100%',
-        full_screen = TRUE,
-        card_header("METHODS"),
-        div(
-          class = 'card-scroll',
-          markdown(
+        )   
+      ),
+    nav_panel(
+      title = 'METHODS', 
+      markdown(
 "
+<div class = 'row'>
+<div class = 'col-md-2'></div>
+<div class = 'col-md-8'>
+
 #### How to understand and use the dashboard
 
 Fecal Indicator Bacteria (FIB) provide information on the potential exposure risk from contact recreation or fish/shellfish consumption from surface waters with high fecal loads.  These indicators are imperfect and provide only a general overview of the potential risk.  Presence of FIBs does not necessarily indicate the presence of fecal pathogens, but their presence can be used to prioritize areas for further investigation or remediation. 
@@ -175,9 +176,11 @@ Freshwater *E. coli* thresholds for EPCHC/Manatee County year/month maps:
 * <span style='color:#E9C318'>__Yellow__</span>: Cell concentrations 126 - 409 CFU / 100 mL
 * <span style='color:#EE7600'>__Orange__</span>: Cell concentrations 410 - 999 CFU / 100 mL
 * <span style='color:#CC3231'>__Red__</span>: Cell concentrations > 999 CFU / 100 mL
+
+</div>
+<div class = 'col-md-2'></div>
+</div>
 "
-            )
-          )
         )
       )
     )
@@ -218,45 +221,69 @@ Freshwater *E. coli* thresholds for EPCHC/Manatee County year/month maps:
       div(
         class = 'resizable-column',
         style = 'width: 33.33%',
-        card(
+        navset_card_underline(
           full_screen = TRUE,
-          height = '100%',
-          navset_tab(
-            nav_panel(
-              "REPORT CARD",
-              fillCol(flex = c(NA, 1),
-                column(12,
-                  shinyWidgets::materialSwitch('segsel1', 'By bay segment?', value = T, width = '100%')
-                ),
-                plotly::plotlyOutput('entmatrix')
-              )
+          nav_panel(
+            "REPORT CARD",
+              shinyWidgets::materialSwitch('segsel1', 'By bay segment?', value = T, width = '100%'),
+              plotly::plotlyOutput('entmatrix', height = '100%')
             ),
-            nav_panel(
-              "Using this tab",
-              div(
-                class = 'card-scroll',
-                markdown('text')
-              )
-            )
+          nav_panel(
+            "Using this tab",
+            markdown(
+"
+The report card and maps are based on *Enterococcus* cell concentrations with results for bay segments and individual sampling stations. Please view the __METHODS__ tab on the [__OVERVIEW__](#overview) page for a description of the score categories in the report card and the map summaries. 
+
+The displayed data for all of the graphics on this page can be selected using the year slider and area selections at the top.  The year/month map tab includes an additional month slider.  By default, all areas are shown.  To remove an area, select it from the menu and hit the backspace key.
+
+The __REPORT CARD__ tab shows the overall score category each year for sampled locations in the bay.  The row for the selected year from the slider at the top of the page is outlined in black. By default, the bay segment scores are shown. The bay segment abbreviations are as follows: 
+
+* __OTB__: Old Tampa Bay
+* __HB__: Hillsborough Bay
+* __MTB__: Middle Tampa Bay
+* __BCB__: Boca Ciega Bay
+* __LTB__: Lower Tampa Bay
+* __MR__: Manatee River
+
+Stations that apply to each bay segment can be shown instead by toggling the switch above the report card.
+
+The report card is also interactive and can be zoomed by clicking the mouse and dragging to an area of interest.  Reset the plot by double-clicking.  An image of the report card can be saved by clicking the camera icon that is shown above the report card when the cursor is over the plot. 
+
+The __MAP BY YEAR__ tab shows a map of the report card grades assigned each bay segment and the individual station grades for the selected year. These are the same values shown in the report card.
+
+Placing the cursor over a bay segment or station will reveal additional information. 
+
+<img src='baywideyrmapex.png' style='width: 30%; display: block; margin: 0 auto;'>   
+
+The __MAP BY YEAR AND MONTH__ tab shows a map of *Enterococcus* concentration categories for the selected year and month. Samples are identified as occurring during wet (circles) or dry (triangles) conditions. See the __METHODS__ tab on the [__OVERVIEW__](#overview) page for a description of the concentration thresholds.
+
+Placing the cursor over a bay segment or station will reveal additional information. 
+
+<img src='baywideyrmomapex.png' style='width: 40%; display: block; margin: 0 auto;'>   
+
+Finally, clicking on a station on the year/month map will create an interactive popup plot for the complete time series of *Enterococcus* concentrations at that location.  The popup plot can be closed by clicking outside the plot area.
+
+<img src='baywidemodalex.png' style='width: 100%; display: block; margin: 0 auto;'> 
+"              
           )
         )
-      ),
+      )
+    ),
   
     div(
       class='right-column', 
       style = 'width: 66.66%',
-      card(
-        full_screen = TRUE,
-        height = '100%',
-        navset_tab(
-          nav_panel(
-            "MAP BY YEAR",
-            "text"
-          ),
-          nav_panel(
-            "MAP BY YEAR AND MONTH",
-            "Year and month map here"
-            )
+      navset_card_underline(
+        full_screen = T,
+        nav_panel(
+          "MAP BY YEAR",
+          leaflet::leafletOutput('entmapyr')
+        ),
+        nav_panel(
+          "MAP BY YEAR AND MONTH",
+          shinyWidgets::sliderTextInput('mosel1', 'Select month:', choices = names(mos), selected = 'Jul', force_edges = T, grid = T, width = '200%'),
+          # column(8, 'Click on a station to view a complete time series')
+          leaflet::leafletOutput('entmap')
           )
         )
       )
@@ -369,32 +396,36 @@ server <- function(input, output, session) {
     areasel1 <- input$areasel1
     segsel1 <- input$segsel1
     yrsel1 <- input$yrsel1
-    
+
     if(!segsel1){
       stas <- enterodata %>%
         dplyr::filter(bay_segment %in% areasel1) %>%
         dplyr::select(station) %>%
         dplyr::distinct() %>%
         dplyr::pull()
-      
-      p <- try(tbeptools::show_fibmatrix(enterodata, stas = stas, yrrng = c(yrmin1, maxyr), 
+
+      p <- try(tbeptools::show_fibmatrix(enterodata, stas = stas, yrrng = c(yrmin1, maxyr),
                                          indic = 'entero', warn = F))
-      
+
     }
-    
+
     if(segsel1){
-      
-      p <- try(tbeptools::show_fibmatrix(enterodata, stas = NULL, bay_segment = areasel1, 
+
+      p <- try(tbeptools::show_fibmatrix(enterodata, stas = NULL, bay_segment = areasel1,
                                          yrrng = c(yrmin1, maxyr), indic = 'entero', warn = F))
-      
+
     }
-    
+
     validate(
       need(!inherits(p, 'try-error'), 'No enterococcus data available for selection')
     )
-    
-    out <- mataddyr_fun(p, yrsel = yrsel1)
-    
+
+    out <- mataddyr_fun(p, yrsel = yrsel1) |>
+      plotly::layout(
+        autosize = TRUE
+      )
+
+
     return(out)
     
   })
@@ -477,7 +508,7 @@ server <- function(input, output, session) {
     if(inherits(enterotomap, 'try-error'))
       leaflet::leafletProxy('entmap') |> 
       leaflet::clearMarkers() 
-    
+
     if(!inherits(enterotomap, 'try-error'))
       leaflet::leafletProxy('entmap') |> 
       leaflet::clearMarkers() |>
@@ -511,7 +542,7 @@ server <- function(input, output, session) {
     yrsel1 <- input$yrsel1
     mosel1 <- input$mosel1
     station <- input$entmap_marker_click$id
-    
+
     req(mosel1)
     mosel1 <- mos[[mosel1]]
     
@@ -848,6 +879,9 @@ server <- function(input, output, session) {
                               wetdry = T, precipdata = catchprecip, temporal_window = 2, wet_threshold = 0.5)
     
   })
+  
+  # baywide ent map popup
+  output$entmappopup <- plotly::renderPlotly(entmappopup())
   
   # epc fib matrix
   output$fibmatrix <- plotly::renderPlotly(fibmatrix())
