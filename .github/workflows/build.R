8a9892e26cdf18c032841ed6c117d9044994c3fb
fawda123 <mbafs2012@gmail.com>
2025-03-23 12:05:41 -0400

    build to .github folder, resave county fib data with compress xz

--- .github/workflows/build.R
+++ .github/workflows/build.R
@@ -4,24 +4,24 @@ library(tbeptools)
 cat('EPC\n')
 xlsx <- tempfile(fileext = '.xlsx')
 fibdata <- read_importfib(xlsx, download_latest = TRUE)
-save(fibdata, file = here::here('data/fibdata.RData'), compress = 'xz')
+save(fibdata, file = here::here('data/fibdata.RData'))
 
 # ESC
 cat('ESC\n')
 hcesdfibdata <- read_importwqp('21FLHESD_WQX', type = 'fib')
-save(hcesdfibdata, file = here::here('data/hcesdfibdata.RData'), compress = 'xz')
+save(hcesdfibdata, file = here::here('data/hcesdfibdata.RData'))
 
 # Manatee County
 cat('manco\n')
 mancofibdata <- read_importwqp('21FLMANA_WQX', type = 'fib')
-save(mancofibdata, file = here::here('data/mancofibdata.RData'), compress = 'xz')
+save(mancofibdata, file = here::here('data/mancofibdata.RData'))
 
 # Pasco County
 cat('pasco\n')
 pascofibdata <- read_importwqp('21FLPASC_WQX', type = 'fib')
-save(pascofibdata, file = here::here('data/pascofibdata.RData'), compress = 'xz')
+save(pascofibdata, file = here::here('data/pascofibdata.RData'))
 
 # Polk County
 cat('polco\n')
 polcofibdata <- read_importwqp('21FLPOLK_WQX', type = 'fib')
-save(polcofibdata, file = here::here('data/polcofibdata.RData'), compress = 'xz')
+save(polcofibdata, file = here::here('data/polcofibdata.RData'))
